46e67a316fd85046ed3dfd99a65307c93a0e8cbb
Undeemiss <jade@hewittfamily.com>
2021-08-16 22:06:12 -0500

    Removed the scrolling lion cub (Rest in peace)

--- main.lua
+++ main.lua
@@ -7,26 +7,11 @@ local input = require "input"
 function love.load()
     bsCanvas = love.graphics.newCanvas(cfg.bs.w, cfg.bs.h)
     tsCanvas = love.graphics.newCanvas(cfg.ts.w, cfg.ts.h)
-    testImage = love.graphics.newImage("textures/test.jpg")
-    testImage:setFilter("linear", "linear", 1)
     love.graphics.setLineStyle("rough")
-    x = 0
-    print(dbg)
-    for a,b in pairs(dbg) do
-        print(a)
-    end
-
-    local testImageScale = cfg.bs.h / testImage:getHeight()
-    testCanvas = love.graphics.newCanvas(testImage:getWidth() * testImageScale, cfg.bs.h)
-    testCanvas:renderTo(
-        function()
-            love.graphics.draw(testImage, 0, 0, 0, testImageScale)
-        end
-    )
-    testImage:setFilter("linear", "linear", 1)
 
     deck = cards.newDeck(5, 3, 13, 2, 2)
     cards.shuffle(deck)
+
     testCard = gfx.newDispCard(cards.draw(deck))
 end
 
@@ -34,7 +19,6 @@ function love.update(dt)
     input.update()
     dbg.tapStatus.update(dt)
 
-    x = (x + .1*dt) % 1
     testCard.roll = testCard.roll + 0.60*dt
 end
 
@@ -44,8 +28,6 @@ function love.draw()
     tsCanvas:renderTo(
         function()
             love.graphics.clear(0,0,0,1)
-            love.graphics.draw(testCanvas, -x*cfg.ts.w, 0)
-            love.graphics.draw(testCanvas, (1-x)*cfg.ts.w, 0)
             testCard:draw()
         end
     )
@@ -54,8 +36,6 @@ function love.draw()
     bsCanvas:renderTo(
         function()
             love.graphics.clear(0,0,0,1)
-            love.graphics.draw(testCanvas, x*cfg.bs.w, 0)
-            love.graphics.draw(testCanvas, (x-1)*cfg.bs.w, 0)
             testCard:draw()
         end
     )
