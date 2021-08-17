3356cc4b3488d2362b5d7de51d41c007d70d2232
Undeemiss <jade@hewittfamily.com>
2021-08-16 22:26:51 -0500

    Added input.cursor.dx and input.cursor.dy

--- input.lua
+++ input.lua
@@ -1,7 +1,9 @@
 input = {
     cursor = {
         x = 0,
+        dx = 0,
         y = 0,
+        dy = 0,
         held = false,    -- (held this frame)
         press = false,   -- (held this frame) and (not held last frame)
         release = false, -- (not held this frame) and (held last frame)
@@ -20,8 +22,12 @@ input.update = function()
     input.cursor.release = (not now) and last
 
     if now then
+        local lastX = input.cursor.x
+        local lastY = input.cursor.y
         input.cursor.x = math.floor(love.mouse.getX() / 3)
         input.cursor.y = math.floor(love.mouse.getY() / 3)
+        input.cursor.dx = input.cursor.x - lastX
+        input.cursor.dy = input.cursor.y - lastY
     end
 end
 
