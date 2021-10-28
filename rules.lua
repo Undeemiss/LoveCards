1e938d5894d6d24c4c1aa16f3881fa4b490a0ef5
Undeemiss <jade@hewittfamily.com>
2021-10-28 17:54:18 -0500

    Passing a turn is now its own function
    Previously, passing a turn was handled directly by rules.round.update,
    but now it is a standalone function. The GUI will be updated to call
    this new method directly soon

--- rules.lua
+++ rules.lua
@@ -29,33 +29,37 @@ rules.round.init = function(cardCount)
     rules.round.currentPlr = 0
 end
 
+rules.round.passTurn = function()
+    -- If this player is the first to get a perfect score, mark the round as ending
+    if (not rules.round.won) and (rules.round.currentPlr > 0) and (scoring.scoreHand(players[rules.round.currentPlr].hand, rules.round.cardCount) == 0) then
+        rules.round.won = true
+        rules.round.countdown = players.size - 1
+        rules.round.scores[rules.round.currentPlr] = 0
+        -- print("Player " .. rules.round.currentPlr .. " won the round; setting countdown to " .. rules.round.countdown) -- Test code
+
+    -- If the round is ending, save the player's score for this round
+    elseif rules.round.won then
+        rules.round.countdown = rules.round.countdown - 1
+        rules.round.scores[rules.round.currentPlr] = scoring.scoreHand(players[rules.round.currentPlr].hand, rules.round.cardCount)
+        -- print("Player " .. rules.round.currentPlr .. " played their last move; reducing countdown to " .. rules.round.countdown) -- Test code
+    end
+
+    -- Increment the turn marker and start the next player's turn
+    if rules.round.won and (rules.round.countdown == 0) then
+        -- TODO: End the round
+    else
+        rules.round.currentPlr = rules.round.currentPlr + 1
+        if rules.round.currentPlr > players.size then
+            rules.round.currentPlr = 1
+        end
+        gui.loadPlr(rules.round.currentPlr)
+    end
+end
+
 rules.round.update = function(dt)
     -- If the turn has ended, pass the turn
     if gui.endedTurn then
-        -- If this player is the first to get a perfect score, mark the round as ending
-        if (not rules.round.won) and (rules.round.currentPlr > 0) and (scoring.scoreHand(players[rules.round.currentPlr].hand, rules.round.cardCount) == 0) then
-            rules.round.won = true
-            rules.round.countdown = players.size - 1
-            rules.round.scores[rules.round.currentPlr] = 0
-            -- print("Player " .. rules.round.currentPlr .. " won the round; setting countdown to " .. rules.round.countdown) -- Test code
-
-        -- If the round is ending, save the player's score for this round
-        elseif rules.round.won then
-            rules.round.countdown = rules.round.countdown - 1
-            rules.round.scores[rules.round.currentPlr] = scoring.scoreHand(players[rules.round.currentPlr].hand, rules.round.cardCount)
-            -- print("Player " .. rules.round.currentPlr .. " played their last move; reducing countdown to " .. rules.round.countdown) -- Test code
-        end
-
-        -- Increment the turn marker and start the next player's turn
-        if rules.round.won and (rules.round.countdown == 0) then
-            -- TODO: End the round
-        else
-            rules.round.currentPlr = rules.round.currentPlr + 1
-            if rules.round.currentPlr > players.size then
-                rules.round.currentPlr = 1
-            end
-            gui.loadPlr(rules.round.currentPlr)
-        end
+        rules.round.passTurn()
     end
 
     -- Update the GUI
